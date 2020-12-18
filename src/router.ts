42afc10abf0908dc25bf1980beeec2642d8a541a
Alex Chiu <alexchiu11@gmail.com>
2020-12-18 07:32:39 +0000

    route tweaks. added subpath for channel ids

--- src/router.ts
+++ src/router.ts
@@ -3,20 +3,25 @@ import Main from "@/components/Main.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/login",
+    path: "/",
     name: "Login/Signup",
     component: () => import("@/components/Auth.vue")
   },
   {
-    path: "/",
+    path: "/channels/:id",
     name: "Main",
     component: Main
-  }
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  if (!to.matched.length) console.warn('no match')
+  next()
+})
 
 export default router;
