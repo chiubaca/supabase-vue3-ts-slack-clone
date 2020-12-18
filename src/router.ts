d66d3be6753f7c7d678acddbd57d8710a6d00152
Alex Chiu <alexchiu11@gmail.com>
2020-12-18 17:45:53 +0000

    Implemented route guards for redirecting to auth or channels depending on session state

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,34 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Main from "@/components/Main.vue";
+import { supabase } from "./lib/supabase";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Login/Signup",
-    component: () => import("@/components/Auth.vue")
+    component: () => import("@/components/Auth.vue"),
+    beforeEnter: (to, from, next) => {
+      
+      // If supabase session is present direct back to channels
+      if(supabase.auth.user()){
+        next("/channels/1")
+      }
+
+      console.log("router got session?", supabase.auth.user())
+      next()
+    }
   },
   {
     path: "/channels/:id",
     name: "Main",
-    component: Main
+    component: () => import("@/components/Main.vue"),
+    beforeEnter: (to, from, next) => {
+      // redirect to login if no user session is present
+      if(supabase.auth.user() === null){
+        next("/")
+      }
+      next()
+    }
+
   },
 ];
 
@@ -19,9 +37,9 @@ const router = createRouter({
   routes
 });
 
-router.beforeEach((to, from, next) => {
-  if (!to.matched.length) console.warn('no match')
-  next()
-})
+// router.beforeEach((to, from, next) => {
+//   if (!to.matched.length) console.warn('no match')
+//   next()
+// })
 
 export default router;
