73e18b01404b9697ae0e378222c4f6304728a86b
Alex Chiu <alexchiu11@gmail.com>
2020-12-19 19:44:34 +0000

    Message component, fetch and show messages in component

---
+++ src/components/Messages.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex  flex-col" v-for="(message, index) in allMessages" :key="index">
+    <span> {{ message.author.username }}: </span>
+    <span> {{ message.message }} </span>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable @typescript-eslint/camelcase */
+import { defineComponent, onMounted } from "vue";
+import {
+  messageListener,
+  fetchMessages,
+  allMessages
+} from "@/vuetils/useMessage";
+export default defineComponent({
+  name: "Channels",
+  async setup() {
+    onMounted(() => {
+      messageListener.subscribe();
+    });
+
+    await fetchMessages(1);
+
+    return { allMessages };
+  }
+});
+</script>
