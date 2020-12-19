df81bdc1793b3848d445c76490f7ab781aa0e4ef
Alex Chiu <alexchiu11@gmail.com>
2020-12-19 21:03:35 +0000

    Created MessageInput component

---
+++ src/components/MessageInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex h-full">
+    <textarea
+      v-model="message"
+      placeholder="Type your message here"
+      class=" border  w-full h-full"
+    ></textarea>
+    <button @click="send" class=" btn-black">Send</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { addMessage } from "@/vuetils/useMessage";
+import { userSession } from "@/vuetils/useAuth";
+
+export default {
+  setup() {
+    const message = ref("");
+
+    function send() {
+      console.log("sending message", message.value);
+      addMessage(message.value, 1, userSession.value.user.id);
+    }
+
+    return { message, send };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
