d60fc7357a5f86f00eff16aef27d29c5b0952865
Alex Chiu <alexchiu11@gmail.com>
2020-12-20 17:20:35 +0000

    Created users component

---
+++ src/components/Users.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-for="(user, index) in allUsers" :key="index">
+    <div class="pt-2">
+      <span class="text-sm text-gray-400">{{ user[0] }}</span>
+      <p>{{ user[1].username }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable @typescript-eslint/camelcase */
+import { defineComponent, onMounted } from "vue";
+import { fetchUsers, userListener, allUsers } from "@/vuetils/useUser";
+export default defineComponent({
+  name: "Channels",
+  async setup() {
+    onMounted(() => {
+      userListener.subscribe();
+    });
+
+    await fetchUsers();
+
+    console.log("got users in component", allUsers.value);
+
+    return { allUsers };
+  }
+});
+</script>
