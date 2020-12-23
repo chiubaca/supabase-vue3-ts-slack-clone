850cb2ffa62c6ab3cbe22ade5d3685af3138d899
Alex Chiu <alexchiu11@gmail.com>
2020-12-23 21:52:30 +0000

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,9 @@
     class="flex flex-col items-center justify-items-center justify-center h-screen"
   >
     <router-view />
-    <Footer />
   </div>
 </template>
 
 <script>
-import Footer from "@/components/Footer.vue";
-import { userSession, handleLogout } from "@/vuetils/useAuth";
-
-export default {
-  components: {
-    Footer
-  },
-  setup() {
-    return { userSession, handleLogout };
-  }
-};
+export default {};
 </script>
